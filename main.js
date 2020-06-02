14386d743cd0e5b965248b20384ec0ffa6dbafd7
yasmeen1999 <yasmeenaleemkhan786@gmail.com>
2020-06-02 18:11:44 +0530

    ES6 and css practice

--- main.js
+++ main.js
@@ -57,3 +57,64 @@ const output = multiplyBy2(num);
 
 
 
+
+// //////object destructuring////////////
+
+// const address ={
+//   street: '',
+//   city:'',
+//   country:''
+// };
+
+// const street = address.street;
+// const city = address.city;
+// const country= address.country;
+ 
+// //////////////destructuring the above object //////////////////
+
+
+//  const{street , city ,country}= address;
+
+
+// array map method
+
+// const colors= ['red','blue','green'];
+// const items = colors.map(colors => `<li>${colors}</li>`);
+// console.log(items);
+
+
+/////////////// spread operators /////////////// combining two operators or array 
+
+// const first = {name:yasmeen};
+// const second = {job:developer};
+
+// const combined = {...first, ...second, location:"india"};
+// console.log(combined);
+
+// const num = 3;
+// const multiplayBy2 = (inputNumber) => {
+//   const result = inputNumber * 2;
+//   return result;
+// };
+// const output = multiplayBy2(num);
+// console.log(output);
+
+
+// const copyArrayAndMultiplyBy2 = (array) =>{
+//   const output = [];
+//   for (let i =0; i <array.length; i++){
+//     output.push(array[i]*2);
+//   }
+//   return output;
+// }
+// const myArray =[1,2,3]
+// const result = copyArrayAndMultiplyBy2(myArray);
+// console.log(result);
+
+const addTwo = (add) => {
+    const output = add+1 ;
+    return output;
+  }
+  const result = addTwo (10);
+  console.log(result);
+  
