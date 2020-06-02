f3797c1f41bc86b40bbf708509fe7026c109ac8d
yasmeen1999 <yasmeenaleemkhan786@gmail.com>
2020-06-02 18:08:07 +0530

    js and float practice

---
+++ main.js
@@ -0,0 +1,59 @@
+
+// var name = "John";
+
+// function first() {
+//   var a = "Hello!";
+//   second();
+//   var x = a + name;
+// }
+
+// function second() {
+//   var b = "Hi!";
+//   third();
+//   var y = b + name;
+// }
+
+// function third() {
+//   var c = "Hey!";
+//   var z = c + name;
+//   console.log(z);
+// }
+
+// first();
+//  array
+// var fruites=["apple","mango",'banana'];
+// console.log(fruites);
+// console.log(fruites[1]);
+
+
+
+// normal function
+
+
+// function sub(a,b){
+//   return a-b;
+// } 
+// var result=sub(3,5);
+// console.log(result);
+
+
+// funtion expresion
+
+
+// var sub=function(a,b){
+//   return a-b;
+// }
+// result(2,1);
+// console.log(sub);
+
+const num = 3;
+const multiplyBy2 = (inputNumber) => {
+ const result = inputNumber*2;
+ return result;
+}
+const output = multiplyBy2(num);
+// const nweOutput = multiplyBy2(10);
+
+
+
+
